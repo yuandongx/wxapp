f8a5277a0158a065e507f21bc90e311f469a51b4
yuandongx <yuandongx@126.com>
2022-02-22 17:52:39 +0800

    设计框架

---
+++ src/view/Right.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="top-line">top</div>
+    <div class="center-line">center</div>
+    <div class="bottom-line">bottom</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
