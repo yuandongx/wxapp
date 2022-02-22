6f7640dde61d6f226fef2cd5fc29f44a81c793ae
yuandongx <yuandongx@126.com>
2022-02-22 17:52:39 +0800

    设计框架
    Signed-off-by: yuandongx <yuandongx@126.com>

---
+++ src/view/Index.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <div class="header border-line">122222223</div>
+  <div>
+    <div class="aside">
+      <div class="left-side border-line content-height">left</div>
+    </div>
+    <div class="right">
+      <div class="center-content content-height  border-line">center</div>
+    </div>
+  </div>
+  <div class="footer border-line ">footer</div>
+</div>
+</template>
+ 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<style scoped>
+.header {
+  height: 5vh;
+}
+.border-line {
+  border-style: dashed;
+  border-width: 2px;
+  border-color: green;
+}
+.content-height {
+  height: 90vh;
+}
+.aside {
+  float: left;
+  width: 10%;
+}
+.right {
+  float: left;
+  width: 90%;
+}
+.footer {
+  height: 5%;
+}
+</style>
