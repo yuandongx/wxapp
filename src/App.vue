909172ff32d468e7f40151a0da64c3903035f452
yuandongx <yuandongx@126.com>
2022-03-03 16:01:45 +0800

    element-plus ui is ok

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Index from './view/index.vue';
-</script>
-
 <template>
-  <Index  />
+  <el-config-provider :locale="locale">
+    <app />
+  </el-config-provider>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 3px;
-}
-</style>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { ElConfigProvider } from 'element-plus'
+
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+  export default defineComponent({
+    components: {
+      ElConfigProvider,
+    },
+    setup() {
+      return {
+        locale: zhCn,
+      }
+    },
+  })
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {resolve} from "path"
+import ElementPlus from 'unplugin-element-plus/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    ElementPlus({
+      useSource: true,
+    }),
+  ]
 })
